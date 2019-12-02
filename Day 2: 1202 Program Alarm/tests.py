751b8ba5cec874b48fe2c2f8420ff119114ceace
raul <raul.muser@stacc.ee>
2019-12-02 16:46:16 +0200

    Fix spacing

--- Day 2: 1202 Program Alarm/tests.py
+++ Day 2: 1202 Program Alarm/tests.py
@@ -9,13 +9,13 @@ class IntCodeTestCase(TestCase):
         self.assertEqual([2, 0, 0, 0, 99], result)
 
     def test_process_code_2(self):
-        result = IntCode.process_code([2,3,0,3,99])
-        self.assertEqual([2,3,0,6,99], result)
+        result = IntCode.process_code([2, 3, 0, 3, 99])
+        self.assertEqual([2, 3, 0, 6, 99], result)
 
     def test_process_code_3(self):
-        result = IntCode.process_code([2,4,4,5,99,0])
-        self.assertEqual([2,4,4,5,99,9801], result)
+        result = IntCode.process_code([2, 4, 4, 5, 99, 0])
+        self.assertEqual([2, 4, 4, 5, 99, 9801], result)
 
     def test_process_code_4(self):
-        result = IntCode.process_code([1,1,1,4,99,5,6,0,99])
-        self.assertEqual([30,1,1,4,2,5,6,0,99], result)
+        result = IntCode.process_code([1, 1, 1, 4, 99, 5, 6, 0, 99])
+        self.assertEqual([30, 1, 1, 4, 2, 5, 6, 0, 99], result)
