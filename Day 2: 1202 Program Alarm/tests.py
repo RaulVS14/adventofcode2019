95d9c952bb951cb055060ba941cfd6463d2aec1f
raul <raul.muser@stacc.ee>
2019-12-02 16:45:33 +0200

    Update tests in Day 2 to use static function process_code

--- Day 2: 1202 Program Alarm/tests.py
+++ Day 2: 1202 Program Alarm/tests.py
@@ -5,21 +5,17 @@ from int_code import IntCode
 class IntCodeTestCase(TestCase):
 
     def test_process_code_1(self):
-        int_code = IntCode()
-        result = int_code.process_code([1, 0, 0, 0, 99])
+        result = IntCode.process_code([1, 0, 0, 0, 99])
         self.assertEqual([2, 0, 0, 0, 99], result)
 
     def test_process_code_2(self):
-        int_code = IntCode()
-        result = int_code.process_code([2,3,0,3,99])
+        result = IntCode.process_code([2,3,0,3,99])
         self.assertEqual([2,3,0,6,99], result)
 
     def test_process_code_3(self):
-        int_code = IntCode()
-        result = int_code.process_code([2,4,4,5,99,0])
+        result = IntCode.process_code([2,4,4,5,99,0])
         self.assertEqual([2,4,4,5,99,9801], result)
 
     def test_process_code_4(self):
-        int_code = IntCode()
-        result = int_code.process_code([1,1,1,4,99,5,6,0,99])
+        result = IntCode.process_code([1,1,1,4,99,5,6,0,99])
         self.assertEqual([30,1,1,4,2,5,6,0,99], result)
